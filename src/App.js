831ec8450f160780e84d970644b1edf18cb946db
amy-yuen <62843939+amy-yuen@users.noreply.github.com>
2020-04-10 17:33:34 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,20 +7,27 @@ import Forum from './components/Forum/Forum';
 import Events from './components/Events/Events';
 import Messages from './components/Messages/Messages';
 import Orgs from './components/Orgs/Orgs';
+import Login from './components/Login/Login';
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router, Route, Redirect, Switch } from "react-router-dom";
 //import { Route } from 'react-router-dom';
 
 function App() {
   return (
+    <Router>
     <div className="App">
       <Header/>
-        {/* 
+      <Switch>
+        <Route exact path="/Login" component={Login} />
+        <Redirect from="/Login" to="/Home" />
+        {/*
         <Route exact={true} path="/" component={Home} />
         <Route path="/Orgs" component={Orgs} />
-         */}
-        
-
-      <Footer/>
+        */}
+      </Switch>
+      <Footer/> 
     </div>
+    </Router>
   );
 }
 
