8d1331c3e149894dd92ec95cde2158a04a8c82e1
amy-yuen <62843939+amy-yuen@users.noreply.github.com>
2020-04-18 12:51:10 -0400

    Update Login.js

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,66 +1,65 @@
 import React, { Component } from "react";
-import 'bootstrap/dist/css/bootstrap.min.css';
 import './Login.css';
+//import FontAwesome from 'react-fontawesome';
+//import faStyles from 'font-awesome/css/font-awesome.css';
 
 export default class Login extends Component {
-  constructor(props) {
-    super(props);
+    constructor(props) {
+      super(props);
+  
+      this.state = {
+        username: "",
+        password: "",
+        loginErrors: ""
+      };
+  
+      this.handleSubmit = this.handleSubmit.bind(this);
+      this.handleChange = this.handleChange.bind(this);
+    }
+  
+    handleChange(event) {
+      this.setState({
+        [event.target.name]: event.target.value
+      });
+    }
+  
+    handleSubmit(event) {
+      const { username, password } = this.state;
+    }
 
-    this.state = {
-      username: "",
-      password: "",
-      loginErrors: ""
-    };
-
-    this.handleSubmit = this.handleSubmit.bind(this);
-    this.handleChange = this.handleChange.bind(this);
-  }
-
-  handleChange(event) {
-    this.setState({
-      [event.target.name]: event.target.value
-    });
-  }
-
-  handleSubmit(event) {
-    const { username, password } = this.state;
-  }
-
-  render() {
-    return (
-      <div className="form">
-        
-        <h1 class="h1 mb-5 font-weight-heavy"></h1>
-          <form className="user" onSubmit={this.handleSubmit}>
-            <img src="https://i.imgur.com/Aug55CS.png" class="logo"></img>
-            <p>
-            <input
-              id="username"
-              name="username"
-              placeholder="Username"
-              class="form-control"
-              value={this.state.username}
-              onChange={this.handleChange}
-              required
-            />
-            </p>
-
-            <p>
-            <input
-              type="password"
-              name="password"
-              placeholder="Password"
-              class="form-control"
-              value={this.state.password}
-              onChange={this.handleChange}
-              required
-            />
-            </p>
-            <p>
-            <button class="btn btn-lg btn-primary btn-block" type="submit">Login</button>
-            </p>
-        </form>
-      </div>
-    );
-  }
-}
+    render() {
+      return (
+        <div class="form">
+            <form class="user" onSubmit={this.handleSubmit}>
+              <a id="pic"><img src="https://i.imgur.com/Aug55CS.png" class="logo"></img></a>
+              <p class="space">
+              <label class="label">Username</label>
+              <input
+                name="username"
+                placeholder="Username"
+                class="input is-success is-rounded is-fullwidth"
+                type="text"
+                value={this.state.username}
+                onChange={this.handleChange}
+                required>
+                </input>
+              </p>
+              <p class="space">
+              <input
+                name="password"
+                placeholder="Password"
+                class="input is-rounded" 
+                type="password"
+                value={this.state.password}
+                onChange={this.handleChange}
+                required
+              />
+              </p>
+              <p class="space">
+              <button class="button is-link is-medium is-overlay is-rounded is-centered" type="submit">Login</button>
+              </p>
+          </form>
+        </div>
+      );
+    }
+    }
