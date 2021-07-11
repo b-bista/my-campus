01f562e93b76e72ec9091899158757a4f2c18c96
Bikranta Bista <bik@Bikrantas-MacBook-Air.local>
2021-07-11 12:39:05 -0400

    updated heroku tapi to https

--- src/constants.js
+++ src/constants.js
@@ -3,7 +3,7 @@ let API = "";
 if (process.env.NODE_ENV === "development") {
   API = "http://localhost:4000";
 } else {
-  API = "http://my-campus-backend.herokuapp.com";
+  API = "https://my-campus-backend.herokuapp.com";
 }
 
 export { API };
